b6d9ed4947c4c13cafadb79994c782f225467f45
Wenbo Zhang <wenbo.zhang@iomesh.com>
2024-02-23 17:13:20 +0800

    feat(helloworld): modify for benchmark
    Signed-off-by: Wenbo Zhang <wenbo.zhang@iomesh.com>

--- examples/src/helloworld/server.rs
+++ examples/src/helloworld/server.rs
@@ -14,20 +14,19 @@ pub struct MyGreeter {}
 impl Greeter for MyGreeter {
     async fn say_hello(
         &self,
-        request: Request<HelloRequest>,
+        _request: Request<HelloRequest>,
     ) -> Result<Response<HelloReply>, Status> {
-        println!("Got a request from {:?}", request.remote_addr());
+        //println!("Got a request from {:?}", request.remote_addr());
 
-        let reply = hello_world::HelloReply {
-            message: format!("Hello {}!", request.into_inner().name),
-        };
+        let buf = Vec::with_capacity(1 << 20);
+        let reply = hello_world::HelloReply { buf };
         Ok(Response::new(reply))
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse().unwrap();
+    let addr = "0.0.0.0:50051".parse().unwrap();
     let greeter = MyGreeter::default();
 
     println!("GreeterServer listening on {}", addr);
